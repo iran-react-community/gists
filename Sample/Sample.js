767925035f1152036a229e6cb8d5aefce44b71c3
aliwesome <aliwsome@gmail.com>
2019-04-21 13:32:31 +0430

    add a sample gist

---
+++ Sample/Sample.js
@@ -0,0 +1,9 @@
+// @name          Sample gist
+// @namespace     http://github.com/iran-react-community/gists/Sample
+// @dependencies  none
+// @type          react functional component
+// @version       0.0.1
+// @description   This is just a sample to show how you can a gist to this repository
+// @author        aliwesome
+
+export default () => <div>Sample</div>
